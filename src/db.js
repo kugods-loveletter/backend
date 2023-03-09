624f30d76c3e3869342ebb5ae3794284fba62dae
Baekgyu <centneuf0109@gmail.com>
2023-03-09 19:59:29 +0900

    add: initialize DB

---
+++ src/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = (error) => console.log("❌ DB Error", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import "./db";
 
 const app = express();
 
