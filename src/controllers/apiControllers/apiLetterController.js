f26c868177eb695694da62d0bbc753b45a545c63
Baekgyu <centneuf0109@gmail.com>
2023-03-10 14:57:58 +0900

    ftr: route Letter

---
+++ src/controllers/apiControllers/apiLetterController.js
@@ -0,0 +1,11 @@
+export const getOneLetter = (req, res) => {};
+
+export const patchOneLetter = (req, res) => {};
+
+export const deleteOneLetter = (req, res) => {};
+
+export const getAllLettersArray = (req, res) => {};
+
+export const getParentLettersArray = (req, res) => {};
+
+export const getDaughterLettersArray = (req, res) => {};
