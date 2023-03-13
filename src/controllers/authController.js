cb49c32d914fafb5d262564095552090ba58a2f8
Baekgyu <centneuf0109@gmail.com>
2023-03-13 17:53:52 +0900

    ftr: loginUserToSession

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,3 +3,8 @@ const { httpResponse } = require("../config/http-response");
 import User from "../models/User";
 import bcrypt from "bcrypt";
 
+const loginUserToSession = (req, user) => {
+    req.session.isLoggedIn = true;
+    req.session.loggedInUser = user;
+};
+
