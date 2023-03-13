41c8ec52fd7abdd850dc9db5de28b4e9257eb731
Baekgyu <centneuf0109@gmail.com>
2023-03-13 17:52:49 +0900

    rft: user->loggedInUser

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 export const saveSessionToLocal = (req, res, next) => {
-    res.locals.loggedInUser = req.session.user;
     res.locals.loggedIn = Boolean(req.session.isLoggedIn);
+    res.locals.loggedInUser = req.session.loggedInUSer;
     next();
 };
