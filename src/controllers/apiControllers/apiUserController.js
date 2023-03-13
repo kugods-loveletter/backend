d540778e7ad6d3a9d3002ae8ba68a718c0b70c68
hyejeong <hyejeong33@gmail.com>
2023-03-13 22:46:23 +0900

    ftr : getAllUsers, getOneUser done

--- src/controllers/apiControllers/apiUserController.js
+++ src/controllers/apiControllers/apiUserController.js
@@ -1,6 +1,43 @@
 const { httpResponse } = require("../../config/http-response");
+import User from "../../models/User";
+import Letter from "../../models/Letter";
 
-export const getAllUsers = (req, res) => {};
-export const getOneUser = (req, res) => {};
-export const getUserSentLetters = (req, res) => {};
-export const getUserReceivedLetters = (req, res) => {};
+export const getAllUsers = async (req, res) => {
+    try {
+        const user = await User.find( null, {"_id":true} );
+        return httpResponse.SUCCESS_OK(res, "", user);
+    } catch (error) {
+        return httpResponse.BAD_REQUEST(res, "", error);
+    }
+};
+
+export const getOneUser = async (req, res) => {
+    const id = req.params.userId;
+    console.log(id);
+    try {
+        const user = await User.find({_id: `${id}`});
+        return httpResponse.SUCCESS_OK(res, "", user);
+    } catch (error) {
+        return httpResponse.BAD_REQUEST(res, "", error);
+    }
+};
+
+export const getUserSentLetters = async (req, res) => {
+    const { id } = req.params.userId;
+    try {
+        const letterId = await Letter.find({senderId: `${id}`}, {_id:true});  
+        return httpResponse.SUCCESS_OK(res, "", letterId);
+    } catch (error) {
+        return httpResponse.BAD_REQUEST(res, "", error);
+    }
+};
+
+export const getUserReceivedLetters = async (req, res) => {
+    const { id } = req.params.userId;
+    try {
+        const letterId = await Letter.find({receiverId: `${id}`}, {_id:true});  
+        return httpResponse.SUCCESS_OK(res, "", letterId);
+    } catch (error) {
+        return httpResponse.BAD_REQUEST(res, "", error);
+    }
+};
