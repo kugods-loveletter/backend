85b96cc10f64649f47364984f79a1e45bd454171
Baekgyu <centneuf0109@gmail.com>
2023-03-13 17:54:16 +0900

    ftr: postSignup

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,3 +12,33 @@ export const getSignup = (req, res) => {
     return res.send("회원가입 페이지 구현을 위한 URL");
 };
 
+export const postSignup = async (req, res) => {
+    const { id, email, pw, confirmPw } = req.body;
+    if (pw !== confirmPw) {
+        return httpResponse.BAD_REQUEST(
+            res,
+            "",
+            "비밀번호와 확인 비밀번호가 같지 않습니다."
+        );
+    }
+    const exists = await User.exists({ $or: [{ id }, { email }] });
+    if (exists) {
+        return httpResponse.BAD_REQUEST(
+            res,
+            "",
+            "입력하신 이메일 또는 아이디를 가진 다른 계정이 이미 존재합니다."
+        );
+    }
+    try {
+        const user = await User.create({
+            id,
+            email,
+            pw,
+        });
+        loginUserToSession(req, user);
+        return httpResponse.SUCCESS_OK(res, "", user);
+    } catch (error) {
+        return httpResponse.BAD_REQUEST(res, "", error);
+    }
+};
+
